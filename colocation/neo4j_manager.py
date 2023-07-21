c27003e502d01248e56a593ef921e57379f3048d
Niklas Molczanski <niklas.molczanski@rwth-aachen.de>
2023-07-21 17:20:08 +0200

    Different authentication

--- colocation/neo4j_manager.py
+++ colocation/neo4j_manager.py
@@ -20,7 +20,7 @@ class Neo4jManager:
         Args:
             password(str): password for the neo4j server if required
         """
-        self.graph = Graph("bolt://localhost:7687", password=password)
+        self.graph = Graph("bolt://localhost:7687", auth=('neo4j', password))
         self.graph.delete_all()
 
     def add_matched_nodes(self, matched: pd.DataFrame, key_w: str, key_c: str, source_w: str, source_c: str):
