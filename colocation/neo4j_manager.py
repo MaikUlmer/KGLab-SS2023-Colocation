4cd8fa9d6aa3d986b2a43b6ca1d1d0346aedd356
Niklas Molczanski <niklas.molczanski@rwth-aachen.de>
2023-07-21 16:25:28 +0200

    Neo4j manager for adding single source matching

---
+++ colocation/neo4j_manager.py
@@ -0,0 +1,68 @@
+'''
+Created on 2023-07-21
+@author: nm
+'''
+from py2neo import Graph, Node, Relationship
+import pandas as pd
+
+
+class Neo4jManager:
+    """
+    Manages importing data and queries to the neo4j graph
+    accumulating the results of the different matching steps.
+    """
+
+    def __init__(self):
+        """
+        constructor
+        connects to neo4j graph and deletes all remaining nodes
+        """
+        self.graph = Graph("bolt://localhost:7687", auth=("", ""))
+        self.graph.delete_all()
+
+    def add_matched_nodes(self, matched: pd.DataFrame, key_w: str, key_c: str, source_w: str, source_c: str):
+        """
+        takes dataframe from a crossover matching by Matcher and adds the match to the database
+        makes sure that nodes and relationships stay unique
+
+        Args:
+            matched(df.DataFrame): match result of a workshop conference match
+            key_w(str): column with the identifier for the workshops
+            key_c(str): column with the identifier for the conferences
+            source_w(str): datasource of the workshops (e.g. wikidata, dblp, ...)
+            source_c(str): datasource of the conferences
+        """
+
+        graph = self.graph
+
+        conference_attr = [s for s in matched.columns if s[0] == "C"]
+
+        workshop_nodes = {}
+        conference_nodes = {}
+
+        tx = graph.begin()
+        for row in matched.drop_duplicates(subset=[f"W.{key_w}"]).to_dict(orient="records"):
+            node = Node(source_w, 'workshop', title=f"{key_w} {row[f'W.{key_w}']}",
+                        **{f"id_{source_w}": row[f"W.{key_w}"]})
+            workshop_nodes[row[f"W.{key_w}"]] = node
+            tx.merge(node, "workshop", f"id_{source_w}")
+
+        for row in matched.drop_duplicates(subset=[f"C.{key_c}"]).to_dict(orient="records"):
+            attributes = {key[2:]: value for key, value in row.items() if key in conference_attr}
+            attributes[f"id_{source_c}"] = row[f"C.{key_c}"]
+            node = Node(source_c, key_c, **attributes)
+            conference_nodes[row[f"C.{key_c}"]] = node
+            tx.merge(node, "conference", key_c)
+
+        graph.commit(tx)
+
+        MATCHES = Relationship.type("MATCHES")
+        tx = graph.begin()
+
+        for row in matched.to_dict(orient="records"):
+            work_node = workshop_nodes[row[f"W.{key_w}"]]
+            conf_node = conference_nodes[row[f"C.{key_c}"]]
+            m = MATCHES(work_node, conf_node)
+            tx.merge(m)
+
+        graph.commit(tx)
---
+++ tests/test_neo4j.py
@@ -0,0 +1,86 @@
+'''
+Created on 2023-07-21
+
+@author: nm
+'''
+import unittest
+import pandas as pd
+from io import StringIO
+from py2neo import Graph
+from colocation.neo4j_manager import Neo4jManager
+
+test_data = """
+;W.number;W.title;W.short;W.month;W.year;W.dates;W.countryISO3;C.conference;C.title;C.countryISO3;C.start;C.end;C.timepoint;C.short;C.month;C.year # noqa: E501
+0;76;29th Intl. Conf. ;VLDB 2003;9.0;2003;['2003', 'September', '2003'];DEU;http://www.wikidata.org/entity/Q113715819;29th International Conference on Very Large Data Bases;DEU;2003-09-09;2003-09-12 00:00:00;2003-09-09;VLDB 2003;9.0;2003.0 # noqa: E501
+1;449;6th European Semantic Web Conference ;ESWC 2009;5.0;2009;['May 31, 2009'];GRC;http://www.wikidata.org/entity/Q64020447;ESWC 2009;GRC;2009-05-31;2009-06-04 00:00:00;2009-05-31;ESWC 2009;5.0;2009.0 # noqa: E501
+2;464;6th European Semantic Web Conference ;ESWC 2009;6.0;2009;['2009', 'June 1st, 2009'];GRC;http://www.wikidata.org/entity/Q64020447;ESWC 2009;GRC;2009-05-31;2009-06-04 00:00:00;2009-05-31;ESWC 2009;5.0;2009.0 # noqa: E501
+3;699;the European Semantic Web Conference 2010 ;ESWC 2010;5.0;2010;['2010'];GRC;http://www.wikidata.org/entity/Q64021290;ESWC 2010;GRC;2010-05-30;2010-06-03 00:00:00;2010-05-30;ESWC 2010;5.0;2010.0 # noqa: E501
+4;1017;25th International Conference on Advanced Information Systems Engineering ;CAiSE 2013;6.0;2013;['2013'];ESP;http://www.wikidata.org/entity/Q106067743;Advanced Information Systems Engineering - 25th International Conference, CAiSE 2013, Valencia, Spain, June 17-21, 2013;ESP;2013-06-17;2013-06-21 00:00:00;2013-06-17;CAiSE 2013;6.0;2013.0 # noqa: E501
+5;1018;International Conference on Very Large Databases ;VLDB 2013;8.0;2013;[];ITA;http://www.wikidata.org/entity/Q113715831;39th International Conference on Very Large Data Bases;ITA;2013-08-26;2013-08-30 00:00:00;2013-08-26;VLDB 2013;8.0;2013.0 # noqa: E501
+6;1027;39th International Conference on Very Large Databases ;VLDB 2013;8.0;2013;[];ITA;http://www.wikidata.org/entity/Q113715831;39th International Conference on Very Large Data Bases;ITA;2013-08-26;2013-08-30 00:00:00;2013-08-26;VLDB 2013;8.0;2013.0 # noqa: E501
+7;1026;9th International Conference on Semantic Systems ;I-SEMANTICS 2013;9.0;2013;[];AUT;http://www.wikidata.org/entity/Q106337563;I-SEMANTICS 2013 - 9th International Conference on Semantic Systems, ISEM '13, Graz, Austria, September 4-6, 2013;AUT;2013-09-04;2013-09-06 00:00:00;2013-09-04;I-SEMANTICS 2013;9.0;2013.0 # noqa: E501
+8;1028;12th International Conference on Perspectives in Business Informatics Research ;BIR 2013;9.0;2013;[];POL;http://www.wikidata.org/entity/Q106079235;Perspectives in Business Informatics Research - 12th International Conference, BIR 2013, Warsaw, Poland, September 23-25, 2013;POL;2013-09-23;2013-09-25 00:00:00;2013-09-23;BIR 2013;9.0;2013.0 # noqa: E501
+9;1030;12th International Semantic Web Conference ;ISWC 2013;10.0;2013;[];AUS;http://www.wikidata.org/entity/Q48025934;The 12th International Semantic Web Conference;AUS;2013-10-21;2013-10-25 00:00:00;2013-10-21;ISWC 2013;10.0;2013.0 # noqa: E501
+10;1034;the 12th International Semantic Web Conference ;ISWC 2013;10.0;2013;[];AUS;http://www.wikidata.org/entity/Q48025934;The 12th International Semantic Web Conference;AUS;2013-10-21;2013-10-25 00:00:00;2013-10-21;ISWC 2013;10.0;2013.0 # noqa: E501
+11;1045;12th International Semantic Web Conference ;ISWC 2013;10.0;2013;[];AUS;http://www.wikidata.org/entity/Q48025934;The 12th International Semantic Web Conference;AUS;2013-10-21;2013-10-25 00:00:00;2013-10-21;ISWC 2013;10.0;2013.0 # noqa: E501
+12;1046;the International Semantic Web Conference ;ISWC 2013;10.0;2013;[];AUS;http://www.wikidata.org/entity/Q48025934;The 12th International Semantic Web Conference;AUS;2013-10-21;2013-10-25 00:00:00;2013-10-21;ISWC 2013;10.0;2013.0 # noqa: E501
+"""
+
+
+class TestNeo4j(unittest.TestCase):
+    """
+    test maintaining neo4j graph fro managing
+    matching data
+    """
+
+    @classmethod
+    def setUpClass(self):
+        self.count_query = """
+            match (n)
+            return count(n) as count
+            """
+        self.graph = Graph("bolt://localhost:7687", auth=("", ""))
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testInit(self):
+        """
+        test whether the manager creates an empty neo4j server
+        """
+        _ = Neo4jManager()
+
+        num = self.graph.run(self.count_query)
+        self.assertEqual(num.evaluate(), 0)
+
+    def testSingleSource(self):
+        """
+        test for a single matchsource, that nodes and relationships can be added
+        adn that no duplicates will occur
+        """
+        neo = Neo4jManager()
+
+        df = pd.read_csv(StringIO(test_data), sep=";")
+        neo.add_matched_nodes(df, "number", "conference", "CeurWS", "Wikidata")
+
+        num = self.graph.run(self.count_query)
+        num = num.evaluate()
+
+        # graph is not empty
+        self.assertGreater(num, 0)
+
+        # repeat insertions does not introduce duplicates
+        neo.add_matched_nodes(df, "number", "conference", "CeurWS", "Wikidata")
+
+        num2 = self.graph.run(self.count_query)
+        num2 = num2.evaluate()
+
+        self.assertEqual(num, num2)
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
